c4cf9b7d0693e14383a61071197740bd143093e7
Camsyn <camsyn@foxmail.com>
2026-06-23 21:53:44 +0800

    refactor(spine): simplify root memory mask check

--- codex-rs/core/src/spine/model/memory.rs
+++ codex-rs/core/src/spine/model/memory.rs
@@ -89,11 +89,9 @@ impl MemRecord {
     pub(in crate::spine) fn allowed_by(&self, raw_mask: RawMask<'_>) -> Result<bool, SpineError> {
         match self.kind {
             MemKind::Suffix => raw_mask.span_live(self.raw_start, self.raw_end),
-            MemKind::RootEpoch => self
-                .raw_live_hash
-                .as_deref()
-                .map(|hash| raw_mask.prefix_hash_matches(self.raw_end, hash))
-                .unwrap_or(Ok(false)),
+            MemKind::RootEpoch => self.raw_live_hash.as_deref().map_or(Ok(false), |hash| {
+                raw_mask.prefix_hash_matches(self.raw_end, hash)
+            }),
         }
     }
 }
