5369d3bfe5f1bc0db68718b9e6bfd1eee78febe7
Camsyn <camsyn@foxmail.com>
2026-07-01 04:57:34 +0800

    refactor(spine): move tree projection helpers behind adapter namespace

--- codex-rs/core/src/session/spine_pressure_prompt.rs
+++ codex-rs/core/src/session/spine_pressure_prompt.rs
@@ -1,9 +1,9 @@
 use super::session::Session;
-use super::spine_tree_inside::SpineTreePressureView;
-use super::spine_tree_inside::build_spine_tree_pressure_view_from_projection;
-use super::spine_tree_inside::node_context_tokens;
 use super::turn_context::TurnContext;
 use crate::spine::SpineCurrentTrimTarget;
+use crate::spine::adapter::projection::SpineTreePressureView;
+use crate::spine::adapter::projection::build_spine_tree_pressure_view_from_projection;
+use crate::spine::adapter::projection::node_context_tokens;
 use crate::spine::bridge::TreeSnapshotProjection;
 use codex_features::Feature;
 use codex_protocol::config_types::ModeKind;
